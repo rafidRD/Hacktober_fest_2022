ed0a2c993542ff1e345257d0197b8f3ada7569c7
Rafid-009 <66445118+Rafid-009@users.noreply.github.com>
2020-10-12 00:04:46 +0600

    Create Go_code.go

---
+++ Master2Code/Hello_World/Go_code.go
@@ -0,0 +1,17 @@
+//First Go program
+package main
+
+
+
+import "fmt"
+
+
+// Main function
+func main() {
+
+
+
+    fmt.Println(" Hello World ")
+}
+//To run the code type the command below: (without '//')
+//go run Go_code.go
